87c6f33bb5f68ef2e7577e12688a18e7da27c3e9
lamhoang195 <bahoang19052004@gmail.com>
2024-11-09 22:26:44 +0700

    Update

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth = Blueprint('auth', __name__)
 
@@ -9,7 +14,12 @@ def signup():
         email = request.form.get('email')
         password = request.form.get('password')
         confirm_password = request.form.get('confirm_password')
-        if len(name) < 3:
+        
+
+        user = User.query.filter_by(email=email).first()
+        if user: 
+            flash('Email already exists.', category='error')
+        elif len(name) < 3:
             flash('Name must be greater than 3 characters.', category='error')
         elif len(email) < 4:
             flash('Mail must be greater than 3 character.', category='error')
@@ -18,17 +28,36 @@ def signup():
         elif password != confirm_password:
             flash('Password and Confirm password must be the same.', category='error')
         else:
+            new_user = User(name=name, email=email, password=generate_password_hash(
+                password, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
             flash('Account created!', category='success')
-            
-    return render_template("signup.html")
+            return redirect(url_for('auth.signup'))
+    return render_template("signup.html", user=current_user)
 
 @auth.route('/signin', methods = ['GET', 'POST'])
 def signin():
-    data = request.form
-    print(data)
-    return render_template("signin.html")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Signed in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again!', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+    return render_template("signin.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<h1>Hello Logout</h1>"
+    logout_user()
+    return redirect(url_for('auth.signin'))
 
--- website/views.py
+++ website/views.py
@@ -1,31 +1,42 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
+
+@views.route('/')
+def index():
+    return redirect(url_for('auth.signin'))
 
 @views.route('/home')
 def home():
     return render_template("index.html")
 
 @views.route('/rsa')
+@login_required
 def rsa():
     return render_template("rsa.html")
 
 @views.route('/elgamal')
+@login_required
 def elgamal():
     return render_template("elgamal.html")
 
 @views.route('/elliptic')
+@login_required
 def elliptic():
     return render_template("elliptic.html")
 
 @views.route('/rsa__signature')
+@login_required
 def rsa_signature():
     return render_template("rsa_signature.html")
 
 @views.route('/elgamal_signature')
+@login_required
 def elgamal_signature():
     return render_template("elgamal_signature.html")
 
 @views.route('/elliptic_signature')
+@login_required
 def elliptic_signature():
     return render_template("elliptic_signature.html")
